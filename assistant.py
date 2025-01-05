b4b549933726f6a60fc3ccc1891517c5094e60eb
Anuj Dutt <anuj.dutt9@gmail.com>
2025-01-05 06:27:51 -0800

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -3,3 +3,8 @@ from langchain_openai import ChatOpenAI
 def get_chatllm_response(question: str, context: str) -> str:
     print(f"Quesion: {question}\nContext: {context}")
     return f"This is a response from the assistant. {context}"
+
+#ToDo: assistant should be able to summarize documents, perform QA on documents as well as answers random queries as well
+
+# ToDo: Define Summarize tool
+# ToDo: Define QA Tool
